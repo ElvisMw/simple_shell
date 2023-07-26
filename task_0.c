0542dd1c2dedb0d5d4a86675e868188d10689b47
ElvisMw <elvismwanthi@gmail.com>
2023-07-26 01:26:40 -0700

    tsk_0

--- task_0.c
+++ task_0.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- *  * main - This is the program's entry point
- *   *
- *    * Description: Prints "This is a beautiful day."
- *     *
- *      * Return: Always 0 (Success)
- *       */
+ * main - This is the program's entry point
+ *
+ * Description: Prints "This is a beautiful day."
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("This is a beautiful day.\n");
